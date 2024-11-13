2ae1dea06982ef59257a36869f4bb9828a016d56
Ruben Dario <torratDev@gmail.com>
2024-11-13 19:26:30 +0100

    FIX: go.yml

--- go/converter_test.go
+++ go/converter_test.go
@@ -9,22 +9,11 @@ import (
 func TestConvertVideoToAudio(t *testing.T) {
 	// Create a temporary directory for input/output files
 	tempDir := t.TempDir()
-	inputPath := filepath.Join(tempDir, "input.mp4")
+	inputPath := filepath.Join("../assets/test_video.mp4")
 	outputPath := filepath.Join(tempDir, "output.mp3")
 
-	// Create an empty video file as a mock input
-	file, err := os.Create(inputPath)
-	if err != nil {
-		t.Fatalf("Failed to create mock video file: %v", err)
-	}
-
-	err = file.Close()
-	if err != nil {
-		return
-	}
-
 	// Run the ConvertVideoToAudio function
-	err = ConvertVideoToAudio(inputPath, outputPath)
+	err := ConvertVideoToAudio(inputPath, outputPath)
 	if err != nil {
 		t.Fatalf("ConvertVideoToAudio failed: %v", err)
 	}
